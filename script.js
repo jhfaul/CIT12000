09a8786ba8e802469192d44c89d25d0b1728c66c
Jared Faul <59935823+jhfaul@users.noreply.github.com>
2020-01-15 16:15:37 -0500

    intro to JS

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 var name = prompt("What is your name?");
-var last = prompt("What is your name?");
-document.body.innerHTML = name+" is my first name. My last name is "+last;
+var color = prompt("What is your favorite color?");
+document.body.innerHTML = "My name is " + name + " and my favorite color is "+color;
